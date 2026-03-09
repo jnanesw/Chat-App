5798f65815859fd94c16eed833c457be5ba01b54
jnanesw <nanimca000@gmail.com>
2026-03-09 19:24:00 +0530

    Updating the Allow origin

--- src/main/java/com/chat/app/config/WebConfig.java
+++ src/main/java/com/chat/app/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://chat-app-backend-916297375578.europe-west1.run.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
